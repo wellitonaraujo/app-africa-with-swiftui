d495c1b2d83338781026611da159aeca8b81d89e
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-04-11 09:49:59 -0300

    screen details init

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,14 +11,20 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         
+        let animals: [Animal] = Bundle.main.docode("animals.json")
+        
         // MARK: - BODY
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             } //: LIST
-            .navigationBarTitle("Africa", displayMode: .large)
+            .navigationBarTitle("Africa", displayMode: .automatic)
             
         } //: NAVIGATION
     }
